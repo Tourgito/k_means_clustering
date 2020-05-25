76fa6b8bf9e8e3bdd8f18762d414d12baa13d962
root <root@localhost.localdomain>
2020-05-25 03:04:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Clustering with K-Means, using Spark
+
+Team Project for the purposes of Big Data course,
+University of Macedonia, Greece
+
+Finding the K-means in a dataset, which contains coordination for a 5-dimensioanl space. Apache Spark was used, where a cluster of nodes was build to calculate the k-means. The process was run multiple times, where in each the cluster had a different number of nodes. Finally, the duration of every execution was written down and printed to a diagram to analyze and compare them.
+
+
+# Members:
+
+George Michoulis - dai16067 / 
+Nikolaos Stefanidis - dai16054 / 
+Dimitris Tourgaidis - dai16057 / 
+Kwstas Tsiwlis - dai16060 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 1986dd1a9e7679fe502c81b3d536e65c45ccdbf8
---
+++ kmeans.py
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+from __future__ import print_function
+
+import sys
+
+import numpy as np
+from pyspark import SparkContext
+from pyspark.mllib.clustering import KMeans
+
+
+def parseVector(line):
+    return np.array([float(x) for x in line.split(',')])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: kmeans <file> <k>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext(appName="KMeans")
+    lines = sc.textFile(sys.argv[1])
+    data = lines.map(parseVector)
+    k = int(sys.argv[2])
+    model = KMeans.train(data, k,initializationMode="random")
+    print("Final centers: " + str(model.clusterCenters))
+    print("Total Cost: " + str(model.computeCost(data)))
+    sc.stop()
+=======
+from __future__ import print_function
+
+import sys
+
+import numpy as np
+from pyspark import SparkContext
+from pyspark.mllib.clustering import KMeans
+
+
+def parseVector(line):
+    return np.array([float(x) for x in line.split(',')])
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: kmeans <file> <k>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext(appName="KMeans")
+    lines = sc.textFile(sys.argv[1])
+    data = lines.map(parseVector)
+    k = int(sys.argv[2])
+    model = KMeans.train(data, k,initializationMode="random")
+    print("Final centers: " + str(model.clusterCenters))
+    print("Total Cost: " + str(model.computeCost(data)))
+    sc.stop()
+
+
+if __name__ == "__main__":
+    main()
+>>>>>>> 1986dd1a9e7679fe502c81b3d536e65c45ccdbf8
